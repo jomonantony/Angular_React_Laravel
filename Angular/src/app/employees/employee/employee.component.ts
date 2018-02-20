562f744898c7f1764a07e3033b361bdb2802513d
Jomon <jomon@oscar3cp.com>
2018-02-20 12:40:46 +0530

    Angular commit2

---
+++ Angular/src/app/employees/employee/employee.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from '../shared/employee.service';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
+import {ToastrService} from 'ngx-toastr';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+
+  constructor(private employeeService : EmployeeService, private toastr : ToastrService) { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+  resetForm(form? : NgForm) {
+    if(form != null)
+    form.reset();
+    this.employeeService.selectedEmployee = {
+      id : null,
+      FirstName : '',
+      LastName : '',
+      EmpCode : '',
+      Position : '',
+      Office : ''
+    }
+  }
+
+  onSubmit(form :NgForm) {
+    if(form.value.id == null) {
+      this.employeeService.postEmployee(form.value)
+      .subscribe(data =>{
+        this.resetForm(form);
+        this.employeeService.getEmployeeList();
+        this.toastr.success('New Record Added sSuiccessfully','Employee Register')
+      })
+    } else {
+      this.employeeService.putEmployee(form.value.id,form.value)
+      .subscribe(data =>{
+        this.resetForm(form);
+        this.employeeService.getEmployeeList();
+        this.toastr.info('Record Updated Successfully','Employee Register')
+      })
+    }
+  }
+}
---
+++ Angular/src/app/employees/shared/employee.model.ts
@@ -0,0 +1,8 @@
+export class Employee {
+    id : number;
+    FirstName : string;
+    LastName : string;
+    EmpCode : string;
+    Position : string;
+    Office : string;
+}
